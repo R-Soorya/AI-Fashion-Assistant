9ffc337a3170fa75fdde3e0f1f05e3d980f28be8
R-Soorya <r.soorya03@gmail.com>
2024-12-19 19:51:15 +0530

    added .env file

--- outfit_recommendation/recommendation_outfit.py
+++ outfit_recommendation/recommendation_outfit.py
@@ -1,8 +1,13 @@
 import google.generativeai as genai
 import json
 from outfit_recommendation import prompt
+import os
+from dotenv import load_dotenv
 
-genai.configure(api_key="AIzaSyD-dGFRJp45jnTHXDzDXf6P46Bok7ZsoX0")
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
--- wardrobe_recommendation/recommendation.py
+++ wardrobe_recommendation/recommendation.py
@@ -2,8 +2,13 @@
 from wardrobe_recommendation import classification, prompt, pairs
 import google.generativeai as genai
 import json
+import os
+from dotenv import load_dotenv
 
-genai.configure(api_key="AIzaSyD-dGFRJp45jnTHXDzDXf6P46Bok7ZsoX0")
+load_dotenv()
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 def recommend():
